5d0d0e7b4fdbe27f167af5cb236cf6ee6016cec7
Glen Egbert <glenegbert1@gmail.com>
2018-12-13 06:14:42 -0700

    setup for aws deploy

---
+++ build/precache-manifest.fead70b3d9843a14e7fce384710a61c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e626ad5621d6d98d971f",
+    "url": "/static/css/main.0f0b4431.chunk.css"
+  },
+  {
+    "revision": "e626ad5621d6d98d971f",
+    "url": "/static/js/main.e626ad56.chunk.js"
+  },
+  {
+    "revision": "11e3838fac0354edfa4e",
+    "url": "/static/js/1.11e3838f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ac6a8b9b9ed4d46fb06c79c496c6670",
+    "url": "/index.html"
+  }
+];
